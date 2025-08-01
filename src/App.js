29cf01eae1a98d9e92cfc5a349ac4cab068ed502
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-08-01 22:49:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GithubSearch from './components/GithubSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch />
+    </div>
+  );
+}
+
+export default App;
